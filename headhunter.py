4257882fa11b16a9da2c60421285ca593ca8a510
dmitriy bragin <>
2022-12-21 19:40:20 +0000

    Initial commit

---
+++ headhunter.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+URL = 'https://hh.ru/search/vacancy?area=113&experience=noExperience&education=not_required_or_not_specified&search_field=name&search_field=company_name&search_field=description&text=Python&from=suggest_post'
+
+def extract_max_page():
+    headers = {
+    'Host': 'hh.ru',
+    'User-Agent': 'Safari',
+    'Accept': '*/*',
+    'Accept-Encoding': 'qzip, deflate, br ',
+    'Connection': 'keep-alive',
+    }
+
+    hh_requests = requests.get(f'{URL}', headers=headers)
+    hh_soup = BeautifulSoup(hh_requests.text, 'html.parser')
+    pages = []
+
+    paginator = hh_soup.find_all("span",{'class': 'pager-item-not-in-short-range' })
+
+    for page in paginator:
+     pages.append(int(page.find('a').text))
+
+ 
+  return pages[-1]
