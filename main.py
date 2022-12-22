f5d382723eb2aeac24492c8080f0411524f1f6ca
Dmitriy <44071732+bragind@users.noreply.github.com>
2022-12-22 19:21:03 +0000

    add

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-from headhunter import extract_max_page
+import requests
+from bs4 import BeautifulSoup
+headers = {
+  'Host': 'hh.ru',
+  'User-Agent': 'Safari',
+  'Accept': '*/*',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Connection': 'keep-alive'
+}
+hh_request = requests.get('https://hh.ru/search/vacancy?no_magic=true&L_save_area=true&text=python&excluded_text=&area=113&salary=&currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_period=0&items_on_page=100', headers=headers)
+hh_soup = BeautifulSoup(hh_request.text, 'html.parser')
 
-max_page = extract_max_page()
-print(max_page)
+paginator = hh_soup.find("span", {'class':'bloko-button_pressed'})
+
+pages = paginator.find_all('a')
+print(pages)
