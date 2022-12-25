091f5570e8de50bcbbcd807afdde548424bd1e17
Dmitriy <44071732+bragind@users.noreply.github.com>
2022-12-25 18:25:50 +0000

    Initial commit

--- headhunter.py
+++ headhunter.py
@@ -1,23 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
-URL = 'https://hh.ru/search/vacancy?area=113&experience=noExperience&education=not_required_or_not_specified&search_field=name&search_field=company_name&search_field=description&text=Python&from=suggest_post'
 
-def extract_max_page():
-    headers = {
-    'Host': 'hh.ru',
-    'User-Agent': 'Safari',
-    'Accept': '*/*',
-    'Accept-Encoding': 'qzip, deflate, br ',
-    'Connection': 'keep-alive',
-    }
+headers = {
+  'Host': 'hh.ru',
+  'User-Agent': 'Safari',
+  'Accept': '*/*',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Connection': 'keep-alive'
+}
 
-    hh_requests = requests.get(f'{URL}', headers=headers)
-    hh_soup = BeautifulSoup(hh_requests.text, 'html.parser')
-    pages = []
+hh_request = requests.get('https://hh.ru/search/vacancy?&items_on_page=100&st=searchVacancy&text=python', headers=headers)
 
-    paginator = hh_soup.find_all("span",{'class': 'pager-item-not-in-short-range' })
+hh_soup = BeautifulSoup(hh_request.text, 'html.parser')
 
-    for page in paginator:
-     pages.append(int(page.find('a').text))
- 
-  return pages[-1]
+pages = []
+
+paginator = hh_soup.find_all("span", {'class':'pager-item-not-in-short-range'})
+
+for page in paginator:
+  pages.append(int(page.find('a').text))
+
+
+max_page = pages[-1]
+for page in range(max_page):
+  print(f'page={page}')
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+
 headers = {
   'Host': 'hh.ru',
   'User-Agent': 'Safari',
@@ -7,10 +8,19 @@ headers = {
   'Accept-Encoding': 'gzip, deflate, br',
   'Connection': 'keep-alive'
 }
-hh_request = requests.get('https://hh.ru/search/vacancy?no_magic=true&L_save_area=true&text=python&excluded_text=&area=113&salary=&currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_period=0&items_on_page=100', headers=headers)
+
+hh_request = requests.get('https://hh.ru/search/vacancy?&items_on_page=100&st=searchVacancy&text=python', headers=headers)
+
 hh_soup = BeautifulSoup(hh_request.text, 'html.parser')
 
-paginator = hh_soup.find("span", {'class':'bloko-button_pressed'})
+pages = []
 
-pages = paginator.find_all('a')
-print(pages)
+paginator = hh_soup.find_all("span", {'class':'pager-item-not-in-short-range'})
+
+for page in paginator:
+  pages.append(int(page.find('a').text))
+
+
+max_page = pages[-1]
+for page in range(max_page):
+  print(f'page={page}')
