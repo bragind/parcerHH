61e6e9446802adce15edd3fb69bf3c0672a39fa4
Dmitriy <44071732+bragind@users.noreply.github.com>
2023-01-09 18:12:03 +0000

    получаем ссылку на вакансии

--- headhunter.py
+++ headhunter.py
@@ -30,6 +30,15 @@ def extract_max_page():
 
   return pages[-1]
 
+def extract_job(html):
+  title = html.find('a').text
+  link = html.find('a')['href']
+  company = html.find('div', {'class': 'vacancy-serp-item__meta-info-company'}).find('a').text
+  company = company.strip()
+  location = html.find('div', {'data-qa': 'vacancy-serp__vacancy-address'}).text
+  location = location.partition(',')[0]
+  return {'title': title, 'company': company, 'location': location, 'link': link}
+
 def extract_hh_jobs(last_page):
   headers = {
   'Host': 'hh.ru',
@@ -38,15 +47,16 @@ def extract_hh_jobs(last_page):
   'Accept-Encoding': 'qzip, deflate, br',
   'Connection': 'keep-alive'}
   jobs =[]
-  #for page in range(last_page):
-  result = requests.get(f'{URL}&page=0', headers = headers)
-  print(result.status_code)
-  soup = BeautifulSoup(result.text, 'html.parser')
-  results = soup.find_all('div',{'class': 'serp-item'})
-  for result in results:
-    title = result.find('a').text
-    company = result.find('div', {'class': 'vacancy-serp-item__meta-info-company'}).find('a').text
-    print(company)
+  for page in range(last_page):
+    print(f'Парсинг страницы {page}')
+    result = requests.get(f'{URL}&page={page}', headers = headers)
+    print(result.status_code)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    results = soup.find_all('div',{'class': 'serp-item'})
+    for result in results:
+      job = extract_job(result)
+      jobs.append(job)
+    
   return jobs
 
 
